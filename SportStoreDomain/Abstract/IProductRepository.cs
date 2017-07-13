1eaaab9d5ce466b77a9ac8fda19fcb317f02156c
Kazumiii <daniel.kruk1@interia.pl>
2017-07-13 20:28:26 +0200

    Update IProductRepository.cs

--- SportStoreDomain/Abstract/IProductRepository.cs
+++ SportStoreDomain/Abstract/IProductRepository.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace SportStoreDomain.Abstract
 {
 
-    //This Interface is responsible for stories my objects .Repository allows us keep persistance logic separte form domain model 
+    //This Interface is responsible for story of my objects .Repository allows us keep persistance logic separte form domain model 
   public  interface IProductRepository
         //IQueryable<T> storing objects without saying anything how or where  our dates are story or ow it will be retirved
     {
