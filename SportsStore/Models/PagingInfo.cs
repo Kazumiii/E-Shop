b6eb92c7fe1bfdf724c6c6859d0ccd8cb4169f82
Szymon Ratajczak <daniel.kruk1@interia.pl>
2018-03-05 08:54:22 +0100

    Update PagingInfo.cs

--- SportsStore/Models/PagingInfo.cs
+++ SportsStore/Models/PagingInfo.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace SportsStore.Models
 {
-    //This class is a view model and provides informations regarding:
+    //This class is a view model and provides informations regarding to:
     public class PagingInfo
     {
         public int TotalItems { get; set; }
